8f9b9b57493d761a25966b4b6cd2b4ea5c0882bd
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-09-30 00:01:26 +0200

    Added methods to retrieve info about user from firestore

--- app/lib/scopedmodels/auth.dart
+++ app/lib/scopedmodels/auth.dart
@@ -1,10 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:hackathon/classes/hive.dart';
+import 'package:hackathon/classes/role.dart';
+import 'package:hackathon/classes/topic.dart';
+import 'package:hackathon/classes/user.dart' as Model;
 import 'package:hackathon/scopedmodels/connected.dart';
 
 mixin AuthModel on ConnectedModel {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Firebase email/password auth methods
 
@@ -45,5 +51,157 @@ mixin AuthModel on ConnectedModel {
 
   signoutGoogle() async {
     await _googleSignIn.signOut();
+  }
+
+  // Cloud Firestore user methods
+
+  Future<Model.User> retrieveUserInfo() async {
+    User user = _auth.currentUser;
+
+    if (user == null) {
+      return null;
+    }
+
+    DocumentSnapshot result =
+        await _firestore.collection('users').doc(user.uid).get();
+
+    if (result != null) {
+      Map<String, dynamic> data = result.data();
+
+      List<Topic> topics = await _retrieveTopicsFromPaths(data['topics']);
+      List<Hive> hives = await _retrieveHivesFromPaths(data['hives']);
+
+      return Model.User(
+        id: result.id,
+        name: data['name'],
+        surname: data['surname'],
+        email: data['email'],
+        bio: data['bio'],
+        topics: topics,
+        hives: hives,
+      );
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<Hive>> _retrieveHivesFromPaths(List<String> paths) async {
+    List<Hive> toReturn = [];
+
+    paths.forEach((path) async {
+      toReturn.add(await _retrieveHiveFromPath(path));
+    });
+
+    return toReturn;
+  }
+
+  Future<Hive> _retrieveHiveFromPath(String path) async {
+    DocumentSnapshot result = await _firestore.doc(path).get();
+
+    if (result != null) {
+      Map<String, dynamic> data = result.data();
+
+      List<OpenRole> openRoles = [];
+      for (dynamic openRole in data['openRoles']) {
+        Role role = await _retrieveRoleFromPath(openRole['roleId']);
+
+        openRoles.add(OpenRole(
+          role: role,
+          quantity: openRole['quantity'],
+        ));
+      }
+
+      List<TakenRole> takenRoles = [];
+      for (dynamic takenRole in data['takenRoles']) {
+        Role role = await _retrieveRoleFromPath(takenRole['roleId']);
+        Model.User user = await _retrieveUserFromPath(takenRole['userId']);
+
+        takenRoles.add(TakenRole(
+          role: role,
+          user: user,
+        ));
+      }
+
+      List<Topic> topics = await _retrieveTopicsFromPaths(data['topics']);
+
+      return Hive(
+        name: data['name'],
+        active: data['active'],
+        description: data['description'],
+        location: data['location'],
+        openRoles: openRoles,
+        takenRoles: takenRoles,
+        topics: topics,
+      );
+    } else {
+      return null;
+    }
+  }
+
+  Future<Model.User> _retrieveUserFromPath(String path) async {
+    DocumentSnapshot result = await _firestore.doc(path).get();
+
+    if (result != null) {
+      Map<String, dynamic> data = result.data();
+
+      List<Topic> topics = await _retrieveTopicsFromPaths(data['topics']);
+
+      return Model.User(
+        id: result.id,
+        name: data['name'],
+        surname: data['surname'],
+        email: data['email'],
+        bio: data['bio'],
+        topics: topics,
+      );
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<Topic>> _retrieveTopicsFromPaths(List<String> paths) async {
+    List<Topic> toReturn = [];
+
+    paths.forEach((path) async {
+      toReturn.add(await _retrieveTopicFromPath(path));
+    });
+
+    return toReturn;
+  }
+
+  Future<Topic> _retrieveTopicFromPath(String path) async {
+    DocumentSnapshot result = await _firestore.doc(path).get();
+
+    if (result != null) {
+      Map<String, dynamic> data = result.data();
+
+      List<Role> roles = await _retrieveRolesFromPaths(data['roles']);
+
+      return Topic(name: data['name'], roles: roles);
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<Role>> _retrieveRolesFromPaths(List<String> paths) async {
+    List<Role> toReturn = [];
+
+    paths.forEach((path) async {
+      toReturn.add(await _retrieveRoleFromPath(path));
+    });
+
+    return toReturn;
+  }
+
+  Future<Role> _retrieveRoleFromPath(String path) async {
+    DocumentSnapshot result = await _firestore.doc(path).get();
+
+    if (result != null) {
+      Map<String, dynamic> data = result.data();
+
+      return Role(name: data['name']);
+    } else {
+      return null;
+    }
   }
 }
