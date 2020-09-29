f56be6686b8177098b4109dc1f162ef24fda6cc8
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-09-29 22:34:48 +0200

    Init user model

---
+++ app/lib/classes/user.dart
@@ -0,0 +1,11 @@
+import 'package:hackathon/classes/hive.dart';
+
+class User {
+  String id;
+  String name;
+  String surname;
+  String email;
+  String bio;
+  List<String> topics;
+  List<Hive> hives;
+}
