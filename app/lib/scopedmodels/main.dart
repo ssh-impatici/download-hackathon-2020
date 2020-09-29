2c3fdffe0ffb471e978f498f79b45478bd961db5
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-09-29 21:48:00 +0200

    Added auth scoped model to main model

--- app/lib/scopedmodels/main.dart
+++ app/lib/scopedmodels/main.dart
@@ -1,7 +1,8 @@
+import 'package:hackathon/scopedmodels/auth.dart';
 import 'package:hackathon/scopedmodels/connected.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class MainModel extends Model with ConnectedModel {}
+class MainModel extends Model with ConnectedModel, AuthModel {}
 
 // class MainModel extends Model with ConnectedModel, AnyModel, SuperModel {}
 // mixin AnyModel on ConnectedModel {}
