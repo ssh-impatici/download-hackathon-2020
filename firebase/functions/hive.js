307600a80a6276cd6bcb0fdb3fc51485a7b70598
Samuele Ferri <samuelexferri@gmail.com>
2020-09-30 18:23:18 +0200

    Add leaveHive function

--- firebase/functions/hive.js
+++ firebase/functions/hive.js
@@ -6,23 +6,26 @@ const Fields = admin.firestore.FieldValue;
 
 module.exports = function(e) {
   e.joinHive = functions.https.onRequest(async (req, res) => {
-    // ##### Remove from open roles
+
     if (req.method !== 'POST' || !req.body)
       return res.status(400).send("Please send a POST request");
 
-    const data = JSON.parse(req.body);
+    const data = req.body;
+
     const hive = await db.doc(data.hiveRef).get();
     if (!hive.exists) return res.status(404).send("Hive not found");
+
+    // ##### Remove from open roles
     // Get role id and check if quantity is enough
     const roles = hive.get("openRoles");
     console.log(roles);
     const roleIndex = roles.findIndex(r => r.name == data.roleRef);
     if (roleIndex < 0) return res.status(404).send("Role not available");
     if (roles[roleIndex].quantity == 1) {
-      // if quantity is 0 remove role from list
+      // If quantity is 0 remove role from list
       roles.splice(roleIndex, 1);
     } else {
-      // if quantity is enough subtract it
+      // If quantity is enough subtract it
       roles[roleIndex].quantity -= 1;
     }
     await db.doc(data.hiveRef).update({
@@ -55,13 +58,78 @@ module.exports = function(e) {
         });
       })
   });
+}
+
+module.exports = function(e) {
+  e.leaveHive = functions.https.onRequest(async (req, res) => {
+
+    if (req.method !== 'POST' || !req.body)
+      return res.status(400).send("Please send a POST request");
+
+    const data = req.body;
+
+    const hive = await db.doc(data.hiveRef).get();
+    if (!hive.exists) return res.status(404).send("Hive not found");
+
+    // ##### Remove from taken roles
+    let roles = hive.get("takenRoles");
+    console.log(roles);
+    let roleIndex = roles.findIndex(r => r.name == data.roleRef);
+    if (roleIndex < 0) return res.status(404).send("Role not available");
+    roles.splice(roleIndex, 1)
+    await db.doc(data.hiveRef).update({
+      takenRoles: roles
+    });
+
+    // ##### Add to open roles
+    // Get role id and get his quantity
+    roles = hive.get("openRoles");
+    console.log(roles);
+    roleIndex = roles.findIndex(r => r.name == data.roleRef);
+    // If there is not in openRoles, quantity is set to 1
+    if (roleIndex < 0) {
+      await db.doc(data.hiveRef).update({
+        openRoles: [
+          ...hive.get("openRoles"),
+          {
+            name: data.roleRef,
+            quantity: 1
+          }
+        ]
+      });
+      // If there is in openRoles, quantity is increased by 1
+    } else {
+      roles[roleIndex].quantity++;
+      await db.doc(data.hiveRef).update({
+        openRoles: roles
+      });
+    }
+
+    // ##### Remove hive to user's hives
+    await db.doc(data.userRef).update({
+      hives: Fields.arrayRemove(data.hiveRef)
+    })
+
+    console.log("DIOOOOOOOOOO");
+    var hiveRef_plain = data.hiveRef.substring(6);
+
+    var dataToSent = db.collection("hives").doc(hiveRef_plain).get()
+      .then(snap => {
+        return res.status(201).send({
+          hiveId: hiveRef_plain,
+          hiveData: snap.data()
+        });
+      })
+  });
 
   e.createHive = functions.https.onRequest(async (req, res) => {
     // Check for POST request
     if (req.method !== "POST")
       return res.status(400).send("Please send a POST request");
 
-    const data = { ...req.body };
+    const data = {
+      ...req.body
+    };
 
     let lat = null;
     let lon = null;
