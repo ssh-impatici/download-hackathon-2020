2f55973a926ed0601ac0b6f963fbbf1a7bbfc31f
lorenzoconti <dev.lorenzoconti@gmail.com>
2020-09-30 16:04:51 +0200

    description_page

--- app/lib/pages/hives.dart
+++ app/lib/pages/hives.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hackathon/classes/hive.dart';
+import 'package:hackathon/classes/role.dart';
 import 'package:hackathon/scopedmodels/main.dart';
+import 'package:hackathon/widgets/hive.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class HivesPage extends StatefulWidget {
@@ -43,28 +45,89 @@ class _HivesPageState extends State<HivesPage> {
 
   Widget hiveWidget(Hive hive) {
     return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey.shade800, borderRadius: BorderRadius.circular(10)),
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      width: MediaQuery.of(context).size.width,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(hive.name),
-                Text('Giorgio Bertolotti'),
-                //Text(hive.creator.fullName),
-                //Text(hive.topics.first.id)
+                Container(
+                  child: Text(
+                    hive.name,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  padding: EdgeInsets.only(bottom: 15),
+                ),
+                Container(
+                  child:
+                      Text(hive.creator != null ? hive.creator.fullName : ''),
+                  padding: EdgeInsets.only(bottom: 20),
+                ),
+                _openRoles(hive.openRoles)
               ],
             ),
           ),
-          Flexible(child: _button())
+          Flexible(child: _button(hive))
         ],
       ),
     );
   }
 
-  Widget _button() {
+  Widget _openRoles(List<OpenRole> list) {
+    return list.length > 1
+        ? Row(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  list.first.name,
+                  style: TextStyle(
+                      color: Colors.grey.shade800,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12),
+                ),
+              ),
+              SizedBox(width: 20),
+              Text(
+                '+${list.length - 1} \t more..',
+                style: TextStyle(color: Colors.yellow, fontSize: 12),
+              )
+            ],
+          )
+        : Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+            decoration: BoxDecoration(
+                color: Colors.yellow, borderRadius: BorderRadius.circular(10)),
+            child: Text(
+              list.first.name,
+              style: TextStyle(
+                  color: Colors.grey.shade800,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12),
+            ),
+          );
+  }
+
+  Widget _button(Hive hive) {
     return IconButton(
-      onPressed: () {},
-      icon: Icon(Icons.arrow_forward_ios),
+      onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => HiveDescription(hive)));
+      },
+      icon: Icon(
+        Icons.arrow_forward_ios,
+        color: Colors.yellow,
+      ),
     );
   }
 }
