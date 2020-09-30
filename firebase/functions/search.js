37f39e5c7af8a151fcfd6b506f41e76ce07404c8
Samuele Ferri <samuelexferri@gmail.com>
2020-09-30 10:54:49 +0200

    Add getHivesMap function

--- firebase/functions/search.js
+++ firebase/functions/search.js
@@ -3,12 +3,40 @@ const admin = require('firebase-admin');
 
 const db = admin.firestore();
 
-module.exports = function (e) {
+module.exports = function(e) {
   e.getHivesList = functions.https.onRequest(async (req, res) => {
     const data = JSON.parse(req.body);
     if (!data) return res.status(400).send("Bad request: no body found");
     // num. of topics in common
     const userTopics = await db.doc(data.userRef).get("topics");
     const virtualHives = await db.collection("hives").where("latitude")
+  });
+}
+
+
+module.exports = function(e) {
+  e.getHivesMap = functions.https.onRequest(async (req, res) => {
+    // Check for POST request
+    if (req.method !== "POST")
+      return res.status(400).send('Please send a POST request');
+
+    const data = JSON.parse(req.body);
+    let zoom = data.zoom
+
+    const hives_filtered_1 = await db.collection('hives')
+      .where('latitude', '<=', data.latitude + zoom)
+      .where('latitude', '>=', data.latitude - zoom)
+      //.where('longitude', '<=', data.longitude + zoom)
+      //.where('longitude', '>=', data.longitude - zoom)
+      .limit(100)
+      .get()
+
+    hives_filtered_2 = hives_filtered_1.docs.map(doc => doc.data())
+
+    res.status(200).send(hives_filtered_2.filter(
+      function(a) {
+        return (a.longitude <= (data.longitude + zoom)) && (a.longitude >= (data.longitude - zoom));
+      }
+    ));
   });
 }
